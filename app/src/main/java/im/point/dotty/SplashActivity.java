03cc901c4ffa5f5c28266771ee1fb5b23b8e2240
Pavel Sokolov <Pavel.Zeppa@yandex.ru>
2018-05-04 17:29:28 +0300

    SplashActivity: splash activity
    - styles update
    - manifest update

---
+++ app/src/main/java/im/point/dotty/SplashActivity.java
@@ -0,0 +1,12 @@
+package im.point.dotty;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
