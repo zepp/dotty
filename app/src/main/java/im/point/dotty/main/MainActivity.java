7d0a5fed390517bab7e99e1937977b83d8e74628
Pavel Sokolov <Pavel.Zeppa@yandex.ru>
2018-05-05 22:18:53 +0300

    ui: no action bar theme

--- app/src/main/java/im/point/dotty/main/MainActivity.java
+++ app/src/main/java/im/point/dotty/main/MainActivity.java
@@ -2,12 +2,15 @@ package im.point.dotty.main;
 
 import android.content.Context;
 import android.content.Intent;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
 import im.point.dotty.R;
 
 public class MainActivity extends AppCompatActivity {
+    private Toolbar toolbar;
 
     public static Intent getIntent(Context context) {
         return new Intent(context, MainActivity.class);
@@ -17,5 +20,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        toolbar = findViewById(R.id.toolbar);
+    }
+
+    @Override
+    protected void onPostCreate(@Nullable Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        setSupportActionBar(toolbar);
     }
 }
