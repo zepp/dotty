0e7aa1b43f2cbcec2965af79bcbfc372f8e2f9d1
Pavel Sokolov <Pavel.Zeppa@yandex.ru>
2018-05-10 18:32:35 +0300

    API is updated
    - GET vs POST
    - error handling

--- app/src/main/java/im/point/dotty/network/LoginReply.java
+++ app/src/main/java/im/point/dotty/network/LoginReply.java
@@ -3,12 +3,21 @@ package im.point.dotty.network;
 import com.google.gson.annotations.SerializedName;
 
 public final class LoginReply {
+    String error;
     String token;
     @SerializedName("csrf_token") String csrfToken;
 
     public LoginReply(String token, String csrfToken) {
         this.token = token;
         this.csrfToken = csrfToken;
+    }
+
+    public LoginReply(String error) {
+        this.error = error;
+    }
+
+    public String getError() {
+        return error;
     }
 
     public String getToken() {
--- app/src/main/java/im/point/dotty/network/PointAPI.java
+++ app/src/main/java/im/point/dotty/network/PointAPI.java
@@ -1,15 +1,15 @@
 package im.point.dotty.network;
 
 import io.reactivex.Observable;
-import retrofit2.http.GET;
+import retrofit2.http.POST;
 import retrofit2.http.Query;
 
 public interface PointAPI {
     String BASE = "https://point.im";
 
-    @GET("api/login")
+    @POST("api/login")
     Observable<LoginReply> login(@Query("login") String login, @Query("password") String password);
 
-    @GET("api/logout")
+    @POST("api/logout")
     Observable<LogoutReply> logout(@Query("csrf_token") String token);
 }
