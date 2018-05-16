2cdadc2bac9b33c2e170250ec72ee31328e47dd8
Pavel Sokolov <pavel.zeppa@yandex.ru>
2018-05-16 21:48:00 +0300

    ui: main activity update
    - view pager is added
    - adapter skeleton

--- app/src/main/java/im/point/dotty/main/MainActivity.java
+++ app/src/main/java/im/point/dotty/main/MainActivity.java
@@ -3,6 +3,11 @@ package im.point.dotty.main;
 import android.content.Context;
 import android.content.Intent;
 import android.support.annotation.Nullable;
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
@@ -11,6 +16,8 @@ import im.point.dotty.R;
 
 public class MainActivity extends AppCompatActivity {
     private Toolbar toolbar;
+    private TabLayout tabLayout;
+    private ViewPager pager;
 
     public static Intent getIntent(Context context) {
         return new Intent(context, MainActivity.class);
@@ -21,11 +28,36 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         toolbar = findViewById(R.id.toolbar);
+        tabLayout = findViewById(R.id.main_tab_layout);
+        pager = findViewById(R.id.main_pager);
     }
 
     @Override
     protected void onPostCreate(@Nullable Bundle savedInstanceState) {
         super.onPostCreate(savedInstanceState);
         setSupportActionBar(toolbar);
+        tabLayout.setupWithViewPager(pager);
+        pager.setAdapter(new Adapter(getSupportFragmentManager()));
+    }
+
+    private class Adapter extends FragmentStatePagerAdapter {
+        Adapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            return null;
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return null;
+        }
+
+        @Override
+        public int getCount() {
+            return 0;
+        }
     }
 }
