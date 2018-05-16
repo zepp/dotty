a6fa8748d7a4e964706db5645564ce4d7f0356c8
Pavel Sokolov <pavel.zeppa@yandex.ru>
2018-05-16 22:09:39 +0300

    ui: new fragments
    - AllFragment to show posts from /all
    - CommentedFragment to show commented posts
    - RecentFragment to show recent posts

---
+++ app/src/main/java/im/point/dotty/main/RecentFragment.java
@@ -0,0 +1,30 @@
+package im.point.dotty.main;
+
+import android.widget.Toast;
+
+import java.util.List;
+
+import im.point.dotty.feed.FeedFragment;
+import im.point.dotty.network.MetaPost;
+import io.reactivex.observers.DisposableSingleObserver;
+
+public final class RecentFragment extends FeedFragment {
+    public RecentFragment() {
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        interactor.getRecent().subscribe(new DisposableSingleObserver<List<MetaPost>>() {
+            @Override
+            public void onSuccess(List<MetaPost> metaPosts) {
+                adapter.setList(metaPosts);
+            }
+
+            @Override
+            public void onError(Throwable e) {
+                Toast.makeText(getContext(), e.getLocalizedMessage(), Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+}
