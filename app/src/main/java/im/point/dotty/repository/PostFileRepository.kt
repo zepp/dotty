026d5d0fdf75673c1a7411454bfc7b58df0e0d13
Pavel Sokolov <pavel.zeppa@yandex.ru>
2021-07-12 21:39:25 +0300

    PostFileRepository update
    - accept only images
    - catch errors

--- app/src/main/java/im/point/dotty/repository/PostFileRepository.kt
+++ app/src/main/java/im/point/dotty/repository/PostFileRepository.kt
@@ -3,12 +3,12 @@
  */
 package im.point.dotty.repository
 
-import android.graphics.Bitmap
 import android.graphics.BitmapFactory
+import android.util.Log
 import im.point.dotty.db.PostFileDao
 import im.point.dotty.model.PostFile
 import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.catch
 import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.withContext
 import okhttp3.OkHttpClient
@@ -19,34 +19,37 @@ class PostFileRepository(private val client: OkHttpClient,
                          private val dao: PostFileDao,
                          private val root: File) {
 
-    fun getPostFiles(postId: String): Flow<List<Bitmap>> {
-        return dao.getPostFiles(postId).map {
-            it.map { file ->
-                with(File(root, "${file.id}.jpg")) {
-                    if (exists()) {
-                        BitmapFactory.decodeFile(absolutePath)
-                    } else {
-                        fetchFile(file, this)
-                    }
-                }
+    fun getPostFiles(postId: String) = dao.getPostFiles(postId)
+        .map { it.toBitmapList() }
+        .catch { Log.e(this::class.simpleName, "image fetch error: ", it) }
+
+    private suspend fun List<PostFile>.toBitmapList() = map { file ->
+        with(File(root, file.id)) {
+            if (exists()) {
+                BitmapFactory.decodeFile(absolutePath)
+            } else {
+                file.fetch(this)
             }
         }
     }
 
-    private suspend fun fetchFile(file: PostFile, path: File) = withContext(Dispatchers.IO) {
+    private suspend fun PostFile.fetch(to: File) = withContext(Dispatchers.IO) {
         root.mkdir()
-        val response = client.newCall(getRequest(file.url)).execute()
+        val response = client.newCall(getRequest(url)).execute()
         if (response.isSuccessful) {
             val bytes = response.body()?.bytes() ?: throw Exception("empty response body")
-            path.writeBytes(bytes)
+            to.writeBytes(bytes)
             BitmapFactory.decodeByteArray(bytes, 0, bytes.size)
-                    ?: throw Exception("failed to decode file")
+                ?: throw Exception("failed to decode file")
         } else {
             throw Exception(response.message())
         }
     }
 
-    private fun getRequest(url: String) = Request.Builder().url(url).build()
+    private fun getRequest(url: String) = Request.Builder()
+        .url(url)
+        .addHeader("accept", "image/*")
+        .build()
 
     suspend fun cleanup() = withContext(Dispatchers.IO) {
         root.deleteRecursively()
